c799626933fa8ac79ffa2f8ada5ec96e3208da65
Ophir Harpaz <ophirharpaz@gmail.com>
2016-05-06 17:00:58 +0300

    added the start_bootstrap files

--- client/js/grayscale.js
+++ client/js/grayscale.js
@@ -9,7 +9,7 @@ function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top").removeClass("top-nav-collapse"); //bla bla
     }
 }
 
