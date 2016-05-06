d80d4ced164f3ab56cfc53ce01cf0374195bf47f
Yishay <yishayf@gmail.com>
2016-05-06 19:15:29 +0300

    saving date details to js file

---
+++ client/js/dateDetails.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Yishay on 5/6/2016.
+ */
+
+function saveDate() {
+    var savedDay = document.getElementById("dayDate").value;
+    var savedMonth = document.getElementById("monthDate").value;
+    var savedYear = document.getElementById("yearDate").value;
+    alert (savedDay);
+
+}
