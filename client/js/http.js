25888ae0c17b1b92c3002606d4768633af0805fe
Ophir Harpaz <ophirharpaz@gmail.com>
2016-05-07 08:43:08 +0300

    httpreq and stills

--- client/js/http.js
+++ client/js/http.js
@@ -6,6 +6,12 @@ var app = angular.module('app', []);
 
 app.controller('httpCtrl', function($scope, $http) {
 
+    $scope.i = 0;
+    $scope.inc = function(i) {
+        $scope.i = i + 1;
+        return $scope.i;
+    };
+
     $http.get('https://getweddy.rapidapi.io/getallsuppliers').success(function (data, status) {
         $scope.allSuppliers = data;
         console.log(data);
